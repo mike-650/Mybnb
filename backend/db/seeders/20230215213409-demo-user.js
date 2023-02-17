df031e376c34991dfb90d1d5dfd323ab60b3301e
MichaelSal <salariosamichael@gmail.com>
2023-02-17 12:35:11 -0800

    edit seeder

--- backend/db/seeders/20230215213409-demo-user.js
+++ backend/db/seeders/20230215213409-demo-user.js
@@ -2,13 +2,14 @@
 const bcrypt = require("bcryptjs");
 
 let options = {};
+options.tableName = 'Users'
 if (process.env.NODE_ENV === 'production') {
   options.schema = process.env.SCHEMA;
 }
-/** @type {import('sequelize-cli').Migration} */
+// /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    options.tableName = 'Users';
+    // options.tableName = 'Users';
 
     await queryInterface.bulkInsert(options, [
       {
@@ -30,7 +31,7 @@ module.exports = {
   },
 
   async down (queryInterface, Sequelize) {
-    options.tableName = 'Users';
+    // options.tableName = 'Users';
     const Op = Sequelize.Op;
     await queryInterface.bulkDelete(options, {
       username: { [Op.in]: ['Demo-lition', 'FakeUser1', 'FakeUser2']}
