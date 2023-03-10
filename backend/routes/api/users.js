b0344e66fa1c7e9aba3b8837928af5c08ba584dd
MichaelSal <salariosamichael@gmail.com>
2023-03-09 18:08:06 -0800

    fixed response body for sign up

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router();
 const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
-const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { setTokenCookie } = require('../../utils/auth');
 const { User } = require('../../db/models');
 
 const validateSignup = [
@@ -66,17 +66,16 @@ router.post(
       }
     }
 
-    const token = setTokenCookie(res, user);
+    setTokenCookie(res, user);
 
     // destructure the user instance
-    return res.json({
+    return res.json({ user: {
       id: user.dataValues.id,
       firstName: user.dataValues.firstName,
       lastName: user.dataValues.lastName,
       email: user.dataValues.email,
-      username: user.dataValues.username,
-      token: token
-    });
+      username: user.dataValues.username
+    }});
   }
 );
 
