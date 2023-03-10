124dadb0b3527fdd905bcfb7b65edf373b8c76aa
MichaelSal <salariosamichael@gmail.com>
2023-03-10 15:48:46 -0800

    added login/signup modals

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,15 @@
-import './index.css';
 import React from 'react';
+
+import './index.css';
+
 import * as ReactDOMClient from 'react-dom/client';
 import { Provider as ReduxProvider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { ModalProvider, Modal } from './context/Modal';
 import App from './App';
-import { restoreCSRF, csrfFetch } from './store/csrf';
 
 import configureStore from './store';
+import { restoreCSRF, csrfFetch } from "./store/csrf";
 import * as sessionActions from './store/session';
 
 const store = configureStore();
@@ -23,10 +26,13 @@ const root = ReactDOMClient.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <ReduxProvider store={store}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ReduxProvider>
+    <ModalProvider>
+      <ReduxProvider store={store}>
+        <BrowserRouter>
+          <App />
+          <Modal />
+        </BrowserRouter>
+      </ReduxProvider>
+    </ModalProvider>
   </React.StrictMode>
 )
