51ca1221d7a7bc360d64a60a897ff2dcc5d93393
MichaelSal <salariosamichael@gmail.com>
2023-03-14 08:59:37 -0700

    added some styling

--- backend/db/seeders/20230220202640-demo-spot.js
+++ backend/db/seeders/20230220202640-demo-spot.js
@@ -21,7 +21,7 @@ module.exports = {
         lng: -118.2437,
         name: "Luxury Apartment",
         description: "This spacious and modern apartment is located in the heart of downtown LA and features breathtaking views of the city skyline.",
-        price: 2500
+        price: 250.20
       },
       {
         ownerId: 2,
@@ -33,7 +33,7 @@ module.exports = {
         lng: -74.006,
         name: "Charming Studio Apartment",
         description: "This cozy and comfortable studio apartment is located in a historic building in the heart of downtown New York, just steps away from all the action.",
-        price: 1500
+        price: 150.25
       },
       {
         ownerId: 3,
@@ -45,7 +45,7 @@ module.exports = {
         lng: -87.6298,
         name: "Elegant Chicago Apartment",
         description: "This stylish and sophisticated apartment is located in the heart of Chicago, just a short walk from the famous Magnificent Mile.",
-        price: 3000
+        price: 300.50
       },
       {
         ownerId: 1,
@@ -57,7 +57,7 @@ module.exports = {
         lng: -122.4194,
         name: "Spacious San Francisco Home",
         description: "This beautiful and spacious home is located in the heart of San Francisco, offering stunning views of the city and easy access to all the attractions.",
-        price: 2000
+        price: 200.75
       },
       {
         ownerId: 2,
@@ -69,7 +69,7 @@ module.exports = {
         lng: -80.1918,
         name: "Modern Miami Condo",
         description: "This sleek and contemporary condo is located in the heart of Miami, offering breathtaking views of the ocean and easy access to all the city's attractions.",
-        price: 2800
+        price: 280.30
       },
       {
         ownerId: 3,
@@ -81,7 +81,19 @@ module.exports = {
         lng: -71.0589,
         name: "Historic Boston Brownstone",
         description: "This charming and characterful brownstone is located in the heart of Boston's historic Beacon Hill neighborhood, just a stone's throw from the city's most famous landmarks.",
-        price: 2200
+        price: 220.90
+      },
+      {
+        ownerId: 1,
+        address: "987 Maple Avenue",
+        city: "New York",
+        state: "NY",
+        country: "United States",
+        lat: 40.7128,
+        lng: -74.0060,
+        name: "Sleek Modern Condo",
+        description: "Located in the heart of Manhattan, this sleek and modern condo features stunning city views, high-end finishes, and convenient access to all of New York's top attractions.",
+        price: 350.75
       }
 
     ]
