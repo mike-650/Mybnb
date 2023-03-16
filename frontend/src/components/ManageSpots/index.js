4cb45c50f89f995df7d70d6a098b2b71b837a4fc
MichaelSal <salariosamichael@gmail.com>
2023-03-16 02:49:47 -0700

    fixed components

--- frontend/src/components/ManageSpots/index.js
+++ frontend/src/components/ManageSpots/index.js
@@ -24,24 +24,25 @@ function ManageSpots() {
       </div>
       <div className="all-spots">
         {spots.map(spot =>
-          <NavLink to={`/spots/${spot.id}`} className='nav-link' key={spot.id}>
-            <div className='all-spots-container'>
-              <img title={`${spot.name}`} src={spot.previewImage} alt='Not Available' className='all-spots-images' />
-              <div className="spot-label">
-                <p className='all-spots-desc'><strong>{spot.city}, {spot.state}</strong></p>
-                <p>{spot.avgRating !== 'no rating available' ? <><i className="fa-solid fa-star"></i> {spot.avgRating % 1 !== 0 ? spot.avgRating : `${spot.avgRating}.0`}</> : 'New'}</p>
+          <div key={spot.id}>
+            <NavLink to={`/spots/${spot.id}`} className='nav-link'>
+              <div className='all-spots-container'>
+                <img title={`${spot.name}`} src={spot.previewImage} alt='Not Available' className='all-spots-images' />
+                <div className="spot-label">
+                  <p className='all-spots-desc'><strong>{spot.city}, {spot.state}</strong></p>
+                  <p>{spot.avgRating !== 'no rating available' ? <><i className="fa-solid fa-star"></i> {spot.avgRating % 1 !== 0 ? spot.avgRating : `${spot.avgRating}.0`}</> : 'New'}</p>
+                </div>
+                <p className='all-spots-desc'><strong>${spot.price}</strong> night</p>
+                <div>
+                  <NavLink to={`/spots/${spot.id}/edit`}>
+                    <button>Update</button>
+                  </NavLink>
+                  <button>Delete</button>
+                </div>
               </div>
-              <p className='all-spots-desc'><strong>${spot.price}</strong> night</p>
-              <div>
-                <NavLink to={`/spots/${spot.id}/edit`}>
-                  <button>Update</button>
-                </NavLink>
-
-                <button>Delete</button>
-
-              </div>
-            </div>
-          </NavLink>)}
+            </NavLink>
+          </div>
+        )}
       </div>
     </div>
   )
