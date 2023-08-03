a9a999e7942cf610d00bdd311895d5b3fd3ea09e
MichaelSal <salariosamichael@gmail.com>
2023-08-02 23:33:34 -0700

    changed both create new spot and demo user to buttons

--- frontend/src/components/Navigation/index.js
+++ frontend/src/components/Navigation/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useHistory } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import ProfileButton from './ProfileButton';
 import './Navigation.css';
 
 function Navigation({ isLoaded }){
   const sessionUser = useSelector(state => state.session.user);
+  const history = useHistory();
   return (
     <ul className='nav-bar'>
         <NavLink exact to="/" id='home-icon'>mybnb</NavLink>
       <div className='profile-create-spot'>
-        { sessionUser ? <NavLink to='/spots/new' id='create-spot'>Create a New Spot</NavLink> : null }
+        {/* { sessionUser ? <NavLink to='/spots/new' id='create-spot'>Create a New Spot</NavLink> : null } */}
+        { sessionUser ? <button id='create-spot' onClick={() => history.push('/spots/new')}>Create a New Spot</button> : null }
       {isLoaded && (
         <ProfileButton user={sessionUser} />
         )}
